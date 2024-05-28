f22aa322e3d809fbf11631eec0cfdf40f654a2d6
eirrena <eirrena@yandex.ru>
2024-05-28 23:33:19 +0300

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const buttton = document.querySelector('button');
+const output = document.querySelector('.output');
+
+buttton.addEventListener('click', function() {
+    output.innerHTML = 'Мама, я тебя люблю!';
+});
